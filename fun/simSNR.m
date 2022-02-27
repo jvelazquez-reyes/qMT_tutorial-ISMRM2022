8af9c6b332aa629bb2e7df5fd5ab32eb36e4ed70
Juan Velazquez <velazquezrj2012@licifug.ugto.mx>
2022-02-26 22:29:29 -0500

    getSim2simRes

--- fun/simSNR.m
+++ fun/simSNR.m
@@ -29,4 +29,23 @@ for i=1:length(SNR)
      dataRaw_SNR(:,1,i) = Protocol.Offsets;
      dataRaw_SNR(:,2,i) = data.MTdata;
  end
- end
+end
+
+%% Function to get the results from the simulations varying different parameters
+function [SimCurveResults,Protocol,data] = getSimResults(Model, x, Opt, data)
+
+
+    Protocol = GetProt(Model);
+    FitOpt   = GetFitOpt(Model,data);
+    % normalize data
+            NoMT = Protocol.Angles<1;
+            if ~any(NoMT)
+                warning('No MToff (i.e. no volumes acquired with Angles=0) --> Fitting assumes that MTData are already normalized.');
+            else
+                data.MTdata = data.MTdata/median(data.MTdata(NoMT));
+                data.MTdata = data.MTdata(~NoMT);
+                Protocol.Angles  = Protocol.Angles(~NoMT)*0.5;
+                Protocol.Offsets = Protocol.Offsets(~NoMT);
+            end
+            SimCurveResults = SPGR_SimCurve(x, Protocol, FitOpt );
+end
